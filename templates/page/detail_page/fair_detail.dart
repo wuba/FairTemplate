14a3e7549d86e7b913317af4a95ac59a03b2a640
lanhuajian <lanhuajian@58.com>
2023-04-11 15:49:32 +0800

    [feature] 模版代码同步更新

--- templates/page/detail_page/fair_detail.dart
+++ templates/page/detail_page/fair_detail.dart
@@ -22,7 +22,7 @@ class _FairDetailState extends State<FairDetail> {
   void onUnload() {}
 
   void requestData() {
-    FairNet().request({
+    FairNet().requestRemoteData({
       'pageName': '#FairKey#',
       'method': 'GET',
       'url':
